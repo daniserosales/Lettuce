b3d8fd6fb90c10f22835a0779b98019ee43fa485
Nastasia777 <kuznenastia@gmail.com>
2023-06-14 07:59:29 +0100

    Added 3lvl functions, added extras to other functions

--- Client/assets/index.js
+++ Client/assets/index.js
@@ -1,82 +1,109 @@
-const listenBtn = document.querySelector("#beginner");
+const beginnerBtn = document.querySelector("#beginner");
+const intermediateBtn = document.querySelector("#intermediate");
+const hardBtn = document.querySelector("#hard");
 const checkBtn = document.querySelector("#submit-word");
 // const resetBtn = document.querySelector("#resetBtn");
 
 const speech = new SpeechSynthesisUtterance();
 
+let inputWord;
+let score = 0;
+
 // addWords.addEventListener("click", addWord);
-listenBtn.addEventListener("click", listenWord);
+beginnerBtn.addEventListener("click", beginListenWord);
+intermediateBtn.addEventListener("click", interListenWord);
+hardBtn.addEventListener("click", hardListenWord);
 checkBtn.addEventListener("click", checkSpelling);
+input.addEventListener("keypress", (e) => {
+  if (e.keyCode === 13) {
+    checkSpelling();
+  }
+});
 //resetBtn.addEventListener("click", function () {
 
 // location.reload();
 
 //});
-async function randomWord() {
-  const response = await fetch("http://localhost:3000/random");
+
+// Beginner section
+async function beginRandomWord() {
+  const response = await fetch(`http://localhost:3000/random/begin`);
   const data = await response.json();
 
   return data.word;
 }
 
-async function checkSpelling() {
-  const input = document.querySelector("#word").value;
+async function beginListenWord() {
+  inputWord = await beginRandomWord();
+  speech.text = `Your word is ${inputWord}`;
+  speech.rate = 0.8;
+  speech.lang = "en-US";
+  speech.volume = 1;
 
-  if (input === inputWord) {
-    return data.word;
-  }
+  document.querySelector("#input").focus();
+
+  window.speechSynthesis.speak(speech);
+  console.log(inputWord);
 }
-// async function addWord() {
-//  const input = document.querySelector("#addInput").value;
-//  const checkData = await fetch (`https://api.dictionaryapi.dev/api/v2/entries/en/${input}`)
-//  const response = await  checkData.json();
 
-//  console.log(response)
-//  const options = {
-//   method: "POST",
-//   headers: {
-//       "Content-Type": "application/json"
-//   },
-//   body: JSON.stringify(response)
+// Inter section
+async function interRandomWord() {
+  const response = await fetch(`http://localhost:3000/random/inter`);
+  const data = await response.json();
+
+  return data.word;
+}
+
+async function interListenWord() {
+  inputWord = await interRandomWord();
+  speech.text = `Your word is ${inputWord}`;
+  speech.rate = 0.8;
+  speech.lang = "en-US";
+  speech.volume = 1;
+
+  document.querySelector("#input").focus();
+
+  window.speechSynthesis.speak(speech);
+  console.log(inputWord);
+}
+
+// Hard section
+async function hardRandomWord() {
+  const response = await fetch(`http://localhost:3000/random/hard`);
+  const data = await response.json();
+
+  return data.word;
+}
+
+async function hardListenWord() {
+  inputWord = await hardRandomWord();
+  speech.text = `Your word is ${inputWord}`;
+  speech.rate = 0.8;
+  speech.lang = "en-US";
+  speech.volume = 1;
+
+  document.querySelector("#input").focus();
+
+  window.speechSynthesis.speak(speech);
+  console.log(inputWord);
+}
+
+// async function checkSpelling() {
+//   const input = document.querySelector("#word").value;
+
+//   if (input === inputWord) {
+//     return data.word;
+//   }
 // }
-//  if (checkData.status == 200){
-//   document.querySelector("#addInput").value = "";
-//     alert("Word(s) have been added");
-//  } else  {alert("word doesn't exists. Please write another")
-//  setTimeout(() => {
-//   alert.textContent = ""
-// }, 4000)}
-// console.log(response)
-// }
-
-// async function addWord() {
-//   const input = document.querySelector("#addInput").value;
-//   const checkData = await fetch (`http://localhost:3000`)
-//   const response = await  checkData.json();
-
-//   console.log(response)
-//   const options = {
-//    method: "POST",
-//    headers: {
-//        "Content-Type": "application/json"
-//    },
-//    body: JSON.stringify(response)
-//  }
-//   if (checkData.status == 200){
-//    document.querySelector("#addInput").value = "";
-//      alert("Word(s) have been added");
-//   } else  {alert("word doesn't exists. Please write another")
-//   setTimeout(() => {
-//    alert.textContent = ""
-//  }, 4000)}
-//  console.log(response)
-//  }
-let inputWord;
 
 async function checkSpelling() {
   const input = document.querySelector("#input").value;
 
-  if (input == inputWord) {
+  // if (inputWord === undefined) {
+  //   speech.text = `Please type a word ${inputWord}`;
+  //   speech.rate = 0.8;
+  // }
+  if (input === inputWord) {
     speech.text = "That's right";
     speech.rate = 0.8;
   } else if (input != inputWord) {
@@ -84,20 +111,6 @@ async function checkSpelling() {
     speech.text = `It is not right!  The correct spelling is ${inputWord}`;
     speech.rate = 0.8;
   }
-
-  window.speechSynthesis.speak(speech);
-}
-
-async function listenWord() {
-  const input = document.querySelector("#input").value;
-
-  inputWord = await randomWord();
-  speech.text = `Your word is ${inputWord}`;
-  speech.rate = 0.8;
-  speech.lang = "en-US";
-  speech.volume = 1;
-
-  document.querySelector("#word").focus();
 
   window.speechSynthesis.speak(speech);
 }
